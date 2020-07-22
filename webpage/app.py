8994e00143aa70fe0b6e94a0f11d30efc2036af7
Beast Mode <vikashbhakta27@gmail.com>
2020-07-21 21:45:59 -0500

    added routes and fixed predictions

--- webpage/app.py
+++ webpage/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, jsonify
 from xgboost import XGBClassifier
 import pandas as pd
 import numpy as np
@@ -14,16 +14,20 @@ model = pickle.load(open( "crime_CatBoost", "rb" ))
 # Route to render index.html template using data from Mongo
 @app.route("/", methods=["GET", "POST"])
 def home():
-    output_message = ""
+    return render_template("index2.html")
 
+@app.route('/api', methods=['POST'])
+def api():
     if request.method == "POST":
-        hour = int(request.form["hour"])
-        premise = request.form["premise"]
-        tract = int(request.form["tract"])
-        month = int(request.form["month"])
-        dayofweek = request.form["dayofweek"]
-        temperature = int(request.form["temperature"])
-        weather = request.form["weather"]
+        _data = request.json
+        print(_data)
+        hour = int(_data["hour"])
+        premise = _data["premise"]
+        tract = int(_data["tract"])
+        month = int(_data["month"])
+        dayofweek = _data["dayofweek"]
+        temperature = int(_data["temperature"])
+        weather = _data["weather"]
 
 
         # data must be converted to df with matching feature names before predict
@@ -36,11 +40,24 @@ def home():
         proba_array = model.predict_proba(data)
         class_probs = zip(classes, proba_array)
 
-        prob_list = []
+        prob_list=[]
         for offense, prob in class_probs:
             prob_list.append(f'{offense} - Probability: {"%.2f"%(prob)}%')
-    
-    return render_template("index2.html",  message = output_message)
+
+        print({"message":output_message, "prob_list": prob_list})
+        return jsonify({"message":output_message, "prob_list": prob_list})
+
+@app.route('/historical_analysis', methods=["GET"])
+def historical_analysis():
+    return render_template("historical_analysis.html")
+
+@app.route('/machine_learning', methods=["GET"])
+def machine_learning():
+    return render_template("machine_learning.html")
+
+@app.route('/data', methods=["GET"])
+def data():
+    return render_template("data.html")
     
 if __name__ == "__main__":
     app.run(debug=True)
